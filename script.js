d711bc55e804454f9206401f1a8dcd98f597904f
Barbora Krejčová <krejcova.barbora96@gmail.com>
2024-10-07 19:07:21 +0200

    fix

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-const jmeno = prompt("Zadej jmeno")
-const vek = Number(prompt("Zadej vek"))
 
-document.body.innerHTML = "<p>" + jmeno + ", " + "věk: " + vek "</p>"
+const jmeno = prompt("Zadej jmeno");
+const vek = Number(prompt("Zadej vek"));
+
+document.body.innerHTML = "<p>" + jmeno + ", " + "věk: " + vek + "</p>"
+
+/* OKSANY RESENI
+const age = prompt('How old are you?', '25');
+const userName = prompt("What's your name?");
+const surName = prompt("What's your surname?");
+// Květoslav Voňavý, věk: 67
+document.body.innerHTML = `<p>${userName} ${surName}, age: ${age}</p>`;
+document.body.innerHTML +=
+    '<p>' + userName + ' ' + surName + ', age: ' + age + '</p>';
+    */
